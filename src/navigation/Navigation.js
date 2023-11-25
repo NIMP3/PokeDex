d7345b2c2c36f035ef401417b42147dbff0400d0
Edwin Yovany <yovany.dev@gmail.com>
2023-11-25 11:21:00 -0500

    [BUILD] (TabNavigation): Designed TabNavigation Icons

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import { Image } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 import Account from '../screens/Account';
 import Favorite from '../screens/Favorite';
 import Pokedex from '../screens/Pokedex';
@@ -9,9 +12,33 @@ const Tab = createBottomTabNavigator()
 export default function Navigation() {
   return (
     <Tab.Navigator>
-        <Tab.Screen name="Pokedex" component={Pokedex} title="Pokedex" />
-        <Tab.Screen name="Favorite" component={Favorite} title="Favorite" />
-        <Tab.Screen name="Account" component={Account} title="Account" />
+        <Tab.Screen name="Favorite" component={Favorite} options={
+          {
+            tabBarLabel: 'Favoritos',
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="heart" color={color} size={size} />
+            ),
+          }
+        }/>
+        <Tab.Screen name="Pokedex" component={Pokedex} title="Pokedex" options={
+          {
+            tabBarLabel: '',
+            tabBarIcon: () => renderPokeball(),
+          }
+        
+        } />
+        <Tab.Screen name="Account" component={Account} title="Account" options={
+          {
+            tabBarLabel: 'Mi cuenta',
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="user" color={color} size={size} />
+            ),
+          }
+        } />
     </Tab.Navigator>
   )
+}
+
+function renderPokeball() {
+  return <Image source={require('../assets/pokeball.png')} style={{ width: 75, height: 75, top: -15}} />
 }
