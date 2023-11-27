84e18e2bfbcc9abb3ca613c2514f11a09df596a8
Edwin Yovany <yovany.dev@gmail.com>
2023-11-27 07:02:14 -0500

    [BUILD] (API): API consumption with fetch

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,7 +1,19 @@
 import { SafeAreaView, Text } from 'react-native'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { getPokemonsApi } from '../api/pokemon'
 
 export default function Pokedex() {
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    const response = await getPokemonsApi();
+    console.log(response);
+  }
+  
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
