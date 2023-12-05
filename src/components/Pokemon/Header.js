996e258c0235beb4680f52abdb54452b7e29283b
Edwin Yovany <yovany.dev@gmail.com>
2023-12-05 09:21:55 -0500

    [BUILD] (Pokemon detail): View & Navigation - Api [getPokemonDetailsByIdApi] - Header

---
+++ src/components/Pokemon/Header.js
@@ -0,0 +1,68 @@
+import { View, Text, StyleSheet, SafeAreaView, Image } from 'react-native'
+import React from 'react'
+import getColorByPokemonType from '../../utils/getColorByPokemonType'
+
+export default function Header(props) {
+    const { name, order, image, type } = props;
+    const pokemonColor = getColorByPokemonType(type);
+
+    const bgStyle = [{ backgroundColor: pokemonColor, ...styles.bg }]
+
+
+    return (
+        <>
+            <View style={bgStyle} />
+            <SafeAreaView style={styles.content}>
+                <View style={styles.header}>
+                    <Text style={styles.titleName}>{name}</Text>
+                    <Text style={styles.order}>#{`${order}`.padStart(3,0)}</Text>
+                </View>
+                <View style={styles.contentImg}>
+                    <Image source={{ uri: image }} style={styles.image} />
+                </View>
+            </SafeAreaView>
+        </>
+    )
+}
+
+export const styles = StyleSheet.create({ 
+    bg: {
+        width: '100%',
+        height: 400,
+        position: 'absolute',
+        borderBottomLeftRadius: 300,
+        borderBottomRightRadius: 300,
+        transform: [ { scaleX: 2 }]
+    },
+    content: {
+        marginHorizontal: 20,
+        marginTop: 30,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingTop: 40,
+    },
+    titleName: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    order: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    contentImg: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: 30
+    },
+    image: {
+        width: 250,
+        height: 300,
+        resizeMode: 'contain',
+    },
+})
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,16 +1,18 @@
 import { StyleSheet, View, Text, Image, TouchableWithoutFeedback } from 'react-native'
 import React from 'react'
 import { capitalize } from 'lodash'
+import { useNavigation } from '@react-navigation/native'
 import getColorByPokemonType  from '../utils/getColorByPokemonType';
 
 export default function PokemonCard(props) {
     const {pokemon} = props;
+    const navigation = useNavigation();
 
     const pokemonColor = getColorByPokemonType(pokemon.type);
     const bgStyles = { backgroundColor: pokemonColor, ...styles.bgStyles };
 
-    const goToPokemon = () => {
-        console.log(`Go to pokemon: ${pokemon.name}`);
+    const goToPokemon = () => { 
+        navigation.navigate('PokemonScreen', { id: pokemon.id});
     }
 
     return (
