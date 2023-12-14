2368d53228711571a95cc20b02bc5a6eccb8c4f4
Edwin Yovany <yovany.dev@gmail.com>
2023-12-14 09:18:11 -0500

    [BUILD] (Advanced forms system): Implementing Libraries [Formik - Yup] - Views [LoginForm - Welcome] - useAuth [Sessions (Custom hooks - React context)

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,15 +1,73 @@
 import { StyleSheet, TextInput, Button, Keyboard, View, Text } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import { user, userDetails } from '../../utils/userDB'
+import useAuth from '../../hooks/useAuth'
 
 export default function LoginForm() {
+  const [error, setError] = useState("");
+  const { login } = useAuth();
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    validateOnChange: false,
+    onSubmit: (values) => {
+      const { username, password } = values
+      if (user.username === username && user.password === password) {
+        login(userDetails)
+        setError('')
+      }
+      else {
+        setError('Usuario o contraseña incorrectos')
+      }
+    }
+  });
+
   return (
     <View>
         <Text style={styles.title}>Iniciar sesión</Text>
-        <TextInput placeholder='Nombre de usuario' style={styles.input}  autoCapitalize='none'/>
-        <TextInput placeholder='Contraseña' style={styles.input} secureTextEntry={true} autoCapitalize='none'/>
-        <Button title='Entrar' onPress={Keyboard.dismiss}/>
+        <TextInput 
+          placeholder='Nombre de usuario' 
+          style={styles.input}  
+          autoCapitalize='none' 
+          value={formik.values.username}
+          onChangeText={(text) => formik.setFieldValue('username', text)}
+        />
+
+        <TextInput 
+          placeholder='Contraseña' 
+          style={styles.input} 
+          secureTextEntry={true} 
+          autoCapitalize='none'
+          value={formik.values.password}
+          onChangeText={(text) => formik.setFieldValue('password', text)}  
+        />
+        
+        <View style={styles.btnForm}>
+          <Button title='Entrar' onPress={formik.handleSubmit}/>
+        </View>
+
+        <Text style={styles.error}>{formik.errors.username}</Text>
+        <Text style={styles.error}>{formik.errors.password}</Text>
+        <Text style={styles.error}>{error}</Text>
     </View>
   )
+}
+
+function initialValues() {
+    return {
+        username: '',
+        password: '',
+    }
+}
+
+function validationSchema() {
+    return {
+        username: Yup.string().required("El nombre de usuario es obligatorio"),
+        password: Yup.string().required("La contraseña es obligatoria"),
+    }
 }
 
 const styles = StyleSheet.create({
@@ -28,4 +86,16 @@ const styles = StyleSheet.create({
         marginRight: 20,
         padding: 10,
     },
+    error: {
+        textAlign: 'center',
+        color: '#f00',
+        fontWeight: 'bold',
+        fontSize: 15,
+        marginTop: 20,
+    },
+    btnForm: {
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 20,
+    }
 })
---
+++ src/context/AuthContext.js
@@ -0,0 +1,33 @@
+import React, { useState, createContext } from "react";
+
+export const AuthContext = createContext(
+    {
+        auth : undefined,
+        login: () => {},
+        logout: () => {}
+    }
+);
+
+export function AuthProvider({ children }) {
+    const [auth, setAuth] = useState(undefined);
+
+    const login = (user) => {
+        setAuth(user);
+    }
+
+    const logout = () => {
+        setAuth(undefined);
+    }
+
+    const valueContext = {
+        auth,
+        login,
+        logout
+    };
+
+    return (
+        <AuthContext.Provider value={valueContext}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
